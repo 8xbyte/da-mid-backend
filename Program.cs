e8249c52194e5475982c9e89073fad3094c9f4e3
micmaclaynd <101046221+micmaclaynd@users.noreply.github.com>
2024-07-19 16:34:20 +0300

    Update startup code Add swagger

--- Program.cs
+++ Program.cs
@@ -1,9 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 app.MapControllers();
+app.UseSwaggerUI();
+app.UseSwagger();
 
 app.Run();
