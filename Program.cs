0fd29ccfc955e2a43fe43a9619c3270ec9975327
micmaclaynd <101046221+micmaclaynd@users.noreply.github.com>
2024-07-19 17:07:54 +0300

    Update startup code Add secret settings json Add jwt options

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
+using DaMid.Interfaces.Options;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile("appsettings.secrets.json");
+builder.Services.Configure<IJwtOptions>(builder.Configuration.GetSection("Jwt"));
 
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
