a1861f46105a672983c4b0489498ec7e3e7bf1a7
micmaclaynd <101046221+micmaclaynd@users.noreply.github.com>
2024-07-19 17:24:45 +0300

    Update startup code Add jwt service

--- Program.cs
+++ Program.cs
@@ -1,9 +1,12 @@
 using DaMid.Interfaces.Options;
+using DaMid.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration.AddJsonFile("appsettings.secrets.json");
 builder.Services.Configure<IJwtOptions>(builder.Configuration.GetSection("Jwt"));
+
+builder.Services.AddScoped<IJwtService, JwtService>();
 
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
