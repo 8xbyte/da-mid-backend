f1dd1bdb361803bf30edad901102f2f894f052e1
micmaclaynd <101046221+micmaclaynd@users.noreply.github.com>
2024-07-20 13:35:36 +0300

    Update application context Add user model

--- Contexts/ApplicationContext.cs
+++ Contexts/ApplicationContext.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using DaMid.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace DaMid.Contexts {
     public class ApplicationContext : DbContext {
         public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
+
+        public DbSet<UserModel> Users { get; set; }
     }
 }
