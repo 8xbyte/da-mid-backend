d3d7a6a64c049251f79fce467ad78bc1063fadce
micmaclaynd <101046221+micmaclaynd@users.noreply.github.com>
2024-07-21 13:44:52 +0300

    Add user service

---
+++ Services/UserService.cs
@@ -0,0 +1,41 @@
+﻿using DaMid.Contexts;
+using DaMid.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DaMid.Services {
+    public interface IUserService {
+        public Task<UserModel?> GetUserById(int id);
+        public Task<UserModel?> GetUserByEmail(string email);
+        public Task<UserModel> CreateUser(UserModel user);
+        public Task<UserModel> UpdateUser(UserModel user);
+        public Task<UserModel> RemoveUser(UserModel user);
+    }
+
+    public class UserService(ApplicationContext context) : IUserService {
+        private readonly ApplicationContext _context = context;
+
+        public async Task<UserModel?> GetUserById(int id) {
+            var user = await _context.Users.FirstOrDefaultAsync(model => model.Id == id);
+            return user;
+        }
+        public async Task<UserModel?> GetUserByEmail(string email) {
+            var user = await _context.Users.FirstOrDefaultAsync(model => model.Email == email);
+            return user;
+        }
+        public async Task<UserModel> CreateUser(UserModel userModel) {
+            var user = await _context.Users.AddAsync(userModel);
+            await _context.SaveChangesAsync();
+            return user.Entity;
+        }
+        public async Task<UserModel> UpdateUser(UserModel userModel) {
+            var user = _context.Users.Update(userModel);
+            await _context.SaveChangesAsync();
+            return user.Entity;
+        }
+        public async Task<UserModel> RemoveUser(UserModel userModel) {
+            var user = _context.Users.Remove(userModel);
+            await _context.SaveChangesAsync();
+            return user.Entity;
+        }
+    }
+}
