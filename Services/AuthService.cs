975fdc8a1051479a3fb32db43d64ba4d5618c7f6
micmaclaynd <101046221+micmaclaynd@users.noreply.github.com>
2024-07-21 21:23:29 +0300

    Add auth service

---
+++ Services/AuthService.cs
@@ -0,0 +1,38 @@
+﻿using DaMid.Contexts;
+using DaMid.Models;
+
+namespace DaMid.Services {
+    public interface IAuthService {
+        public Task<UserModel?> Login(string email, string password);
+        public Task<UserModel?> Register(string email, string password);
+    }
+
+    public class AuthService(IUserService userService) : IAuthService {
+        private readonly IUserService _userService = userService;
+
+        public async Task<UserModel?> Login(string email, string password) {
+            var user = await _userService.GetUserByEmail(email);
+            if (user == null) {
+                return null;
+            }
+
+            if (BCrypt.Net.BCrypt.Verify(password, user.Password)) {
+                return user;
+            }
+
+            return null;
+        }
+        public async Task<UserModel?> Register(string email, string password) {
+            var user = await _userService.GetUserByEmail(email);
+            if (user != null) {
+                return null;
+            }
+
+            var createdUser = await _userService.CreateUser(new UserModel {
+                Email = email,
+                Password = BCrypt.Net.BCrypt.HashPassword(password)
+            });
+            return createdUser;
+        }
+    }
+}
