c6dc643467c9e683225ba7fc4ae7b7a28ac98211
micmaclaynd <101046221+micmaclaynd@users.noreply.github.com>
2024-08-07 23:00:49 +0300

    Update base and user models Add data annotations

--- Models/BaseModel.cs
+++ Models/BaseModel.cs
@@ -1,5 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace DaMid.Models {
     public class BaseModel {
+        [Key]
+        [Required]
+        [Column("id")]
         public int Id { get; set; }
     }
 }
--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -1,12 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace DaMid.Models {
     public enum UserRole {
         User, Admin
     }
 
+    [Table("users")]
     public class UserModel : BaseModel {
+        [Required]
+        [Column("login")]
         public string Login { get; set; }
+
+        [Required]
+        [Column("password")]
         public string Password { get; set; }
+
+        [Required]
+        [Column("role")]
         public UserRole Role { get; set; }
+
+        [Required]
+        [Column("registration_date")]
         public DateTime RegistrationDate { get; set; }
     }
 }
