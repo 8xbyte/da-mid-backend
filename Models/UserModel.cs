b9c835648ea01b461313eab4eb5550d7490f0561
micmaclaynd <101046221+micmaclaynd@users.noreply.github.com>
2024-08-07 22:50:06 +0300

    Update user model Change type of role field

--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -1,8 +1,12 @@
 namespace DaMid.Models {
+    public enum UserRole {
+        User, Admin
+    }
+
     public class UserModel : BaseModel {
         public string Login { get; set; }
         public string Password { get; set; }
-        public string Role { get; set; }
+        public UserRole Role { get; set; }
         public DateTime RegistrationDate { get; set; }
     }
 }
