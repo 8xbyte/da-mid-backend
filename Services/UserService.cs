a3bcf39d5796e4ff6b3a9cfe8f2926523623dd4e
micmaclaynd <101046221+micmaclaynd@users.noreply.github.com>
2024-08-07 23:36:08 +0300

    Update user service Add async in functions names

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -4,33 +4,33 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DaMid.Services {
     public interface IUserService {
-        public Task<UserModel?> GetUserById(int id);
-        public Task<UserModel?> GetUserByLogin(string login);
-        public Task<UserModel> CreateUser(UserModel user);
-        public Task<UserModel> UpdateUser(UserModel user);
-        public Task<UserModel> RemoveUser(UserModel user);
+        public Task<UserModel?> GetUserByIdAsync(int id);
+        public Task<UserModel?> GetUserByLoginAsync(string login);
+        public Task<UserModel> CreateUserAsync(UserModel user);
+        public Task<UserModel> UpdateUserAsync(UserModel user);
+        public Task<UserModel> RemoveUserAsync(UserModel user);
     }
 
     public class UserService(ApplicationContext context) : IUserService {
         private readonly ApplicationContext _context = context;
 
-        public async Task<UserModel?> GetUserById(int id) {
+        public async Task<UserModel?> GetUserByIdAsync(int id) {
             return await _context.Users.FirstOrDefaultAsync(model => model.Id == id);
         }
-        public async Task<UserModel?> GetUserByLogin(string login) {
+        public async Task<UserModel?> GetUserByLoginAsync(string login) {
             return await _context.Users.FirstOrDefaultAsync(model => model.Login == login);
         }
-        public async Task<UserModel> CreateUser(UserModel userModel) {
+        public async Task<UserModel> CreateUserAsync(UserModel userModel) {
             var user = await _context.Users.AddAsync(userModel);
             await _context.SaveChangesAsync();
             return user.Entity;
         }
-        public async Task<UserModel> UpdateUser(UserModel userModel) {
+        public async Task<UserModel> UpdateUserAsync(UserModel userModel) {
             var user = _context.Users.Update(userModel);
             await _context.SaveChangesAsync();
             return user.Entity;
         }
-        public async Task<UserModel> RemoveUser(UserModel userModel) {
+        public async Task<UserModel> RemoveUserAsync(UserModel userModel) {
             var user = _context.Users.Remove(userModel);
             await _context.SaveChangesAsync();
             return user.Entity;
