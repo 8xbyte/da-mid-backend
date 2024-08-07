51fbbcdb2e57c0e9265c4cf2bd2f3dfb3cf60011
micmaclaynd <101046221+micmaclaynd@users.noreply.github.com>
2024-08-07 21:05:31 +0300

    Add auth data interfaces

---
+++ Interfaces/Data/IAuthData.cs
@@ -0,0 +1,11 @@
+namespace DaMid.Interfaces.Data {
+    public class ILoginData {
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class IRegisterData {
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
