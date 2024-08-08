8dc9b036d04fb7c962204ba20221fc3ae0f0286a
micmaclaynd <101046221+micmaclaynd@users.noreply.github.com>
2024-08-08 15:57:54 +0300

    Update startup code Add audience service

--- Program.cs
+++ Program.cs
@@ -13,6 +13,7 @@ builder.Services.AddScoped<IJwtService, JwtService>();
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<IClassService, ClassService>();
+builder.Services.AddScoped<IAudienceService, AudienceService>();
 
 builder.Services.AddDbContext<ApplicationContext>(options => {
     var database = builder.Configuration.GetValue<string>("Database:ConnectionString");
