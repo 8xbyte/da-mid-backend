ff5439d053e8b94419cd19d154f2ea2d564d35b8
micmaclaynd <101046221+micmaclaynd@users.noreply.github.com>
2024-08-08 19:37:58 +0300

    Rename class service to subject service

---
+++ Services/SubjectService.cs
@@ -0,0 +1,46 @@
+using DaMid.Contexts;
+using DaMid.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DaMid.Services {
+    public interface ISubjectService {
+        public Task<SubjectModel?> GetSubjectByIdAsync(int id);
+        public Task<SubjectModel?> GetSubjectByNameAsync(string name);
+        public Task<List<SubjectModel>> GetSubjectsAsync(int offset, int limit);
+        public Task<List<SubjectModel>> SearchSubjectsAsync(string name, int limit);
+        public Task<SubjectModel> AddSubjectAsync(SubjectModel classModel);
+        public Task<SubjectModel> RemoveSubjectAsync(SubjectModel classModel);
+    }
+
+    public class SubjectService(ApplicationContext context) : ISubjectService {
+        private readonly ApplicationContext _context = context;
+
+        public async Task<SubjectModel?> GetSubjectByIdAsync(int id) {
+            return await _context.Subjects.FirstOrDefaultAsync(model => model.Id == id);
+        }
+
+        public async Task<SubjectModel?> GetSubjectByNameAsync(string name) {
+            return await _context.Subjects.FirstOrDefaultAsync(model => model.Name == name);
+        }
+
+        public async Task<List<SubjectModel>> GetSubjectsAsync(int offset, int limit) {
+            return await _context.Subjects.OrderBy(model => model.Id).Skip(offset).Take(limit).ToListAsync();
+        }
+
+        public async Task<List<SubjectModel>> SearchSubjectsAsync(string name, int limit) {
+            return await _context.Subjects.Where(model => EF.Functions.Like(model.Name.ToLower(), $"%{name.ToLower()}%")).OrderBy(model => model.Id).Take(limit).ToListAsync();
+        }
+
+        public async Task<SubjectModel> AddSubjectAsync(SubjectModel classModel) {
+            var _class = await _context.Subjects.AddAsync(classModel);
+            await _context.SaveChangesAsync();
+            return _class.Entity;
+        }
+
+        public async Task<SubjectModel> RemoveSubjectAsync(SubjectModel classModel) {
+            var _class = _context.Subjects.Remove(classModel);
+            await _context.SaveChangesAsync();
+            return _class.Entity;
+        }
+    }
+}
