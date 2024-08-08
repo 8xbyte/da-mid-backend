449540da0bb8ec8050c1decf5e4ac61d520907ee
micmaclaynd <101046221+micmaclaynd@users.noreply.github.com>
2024-08-08 11:39:35 +0300

    Update token payload interface Add role field

--- Interfaces/TokenPayload.cs
+++ Interfaces/TokenPayload.cs
@@ -1,5 +1,8 @@
+using DaMid.Models;
+
 namespace DaMid.Interfaces {
     public class ITokenPayload {
         public required int UserId { get; set; }
+        public required UserRole Role { get; set; }
     }
 }
