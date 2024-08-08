8d5b4865015bf79682976f83f3dac9a86d5dfbca
micmaclaynd <101046221+micmaclaynd@users.noreply.github.com>
2024-08-08 20:43:01 +0300

    Add "AddClassModel" migration

---
+++ Migrations/20240808173453_AddClassModel.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace DaMid.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddClassModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "classes",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    subject_id = table.Column<int>(type: "int", nullable: false),
+                    audience_id = table.Column<int>(type: "int", nullable: false),
+                    teacher_id = table.Column<int>(type: "int", nullable: false),
+                    date = table.Column<DateOnly>(type: "date", nullable: false),
+                    time = table.Column<TimeOnly>(type: "time", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_classes", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_classes_audiences_audience_id",
+                        column: x => x.audience_id,
+                        principalTable: "audiences",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_classes_subjects_subject_id",
+                        column: x => x.subject_id,
+                        principalTable: "subjects",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_classes_teachers_teacher_id",
+                        column: x => x.teacher_id,
+                        principalTable: "teachers",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_classes_audience_id",
+                table: "classes",
+                column: "audience_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_classes_subject_id",
+                table: "classes",
+                column: "subject_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_classes_teacher_id",
+                table: "classes",
+                column: "teacher_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "classes");
+        }
+    }
+}
