52860ec8a3c3b15acc3287882d3bf554c47ae3db
micmaclaynd <101046221+micmaclaynd@users.noreply.github.com>
2024-08-08 20:42:22 +0300

    Add class model

---
+++ Models/ClassModel.cs
@@ -0,0 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DaMid.Models {
+    [Table("classes")]
+    public class ClassModel : BaseModel {
+        [Required]
+        [Column("subject_id")]
+        public int SubjectId { get; set; }
+        public SubjectModel Subject { get; set; }
+
+        [Required]
+        [Column("audience_id")]
+        public int AudienceId { get; set;}
+        public AudienceModel Audience { get; set; }
+
+        [Required]
+        [Column("teacher_id")]
+        public int TeacherId { get; set;}
+        public TeacherModel Teacher { get; set; }
+
+        [Required]
+        [Column("date")]
+        public required DateOnly Date { get; set; }
+
+        [Required]
+        [Column("time")]
+        public required TimeOnly Time { get; set; }
+    }
+}
