ebfdd86c331ebfb1df3f17ba676e6d7afbed3ce4
micmaclaynd <101046221+micmaclaynd@users.noreply.github.com>
2024-08-08 14:02:02 +0300

    Add authorization attribute

---
+++ Attributes/AuthorizationAttribute.cs
@@ -0,0 +1,42 @@
+using DaMid.Interfaces.Options;
+using DaMid.Models;
+using DaMid.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace DaMid.Attributes {
+    [AttributeUsage(AttributeTargets.Method)]
+    public class AuthorizationAttribute(UserRole role) : Attribute, IAuthorizationFilter {
+        public void OnAuthorization(AuthorizationFilterContext context) {
+            var jwtService = context.HttpContext.RequestServices.GetService<IJwtService>();
+            var cookieOptions = context.HttpContext.RequestServices.GetService<IOptions<ICookieOptions>>()?.Value;
+
+            var token = context.HttpContext.Request.Cookies[cookieOptions!.JwtToken];
+            if (token.IsNullOrEmpty()) {
+                context.Result = new UnauthorizedObjectResult(new {
+                    Status = "error",
+                    Result = "not found token"
+                }); return;
+            }
+
+            var tokenPayload = jwtService!.VerifyToken(token!);
+            if (tokenPayload == null) {
+                context.Result = new UnauthorizedObjectResult(new {
+                    Status = "error",
+                    Result = "bad token"
+                }); return;
+            }
+
+            if (tokenPayload!.Role < role) {
+                context.Result = new BadRequestObjectResult(new {
+                    Status = "error",
+                    Result = "access denied"
+                }); return;
+            }
+
+            context.HttpContext.Items.Add(cookieOptions.JwtToken, tokenPayload);
+        }
+    }
+}
