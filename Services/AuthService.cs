8c8263fec2f1dad69c9dc94e4182ba9abfa25086
micmaclaynd <101046221+micmaclaynd@users.noreply.github.com>
2024-08-08 13:56:50 +0300

    Update auth service Now LoginAsync return token otherwise null Now RegisterAsync return token otherwise null

--- Services/AuthService.cs
+++ Services/AuthService.cs
@@ -1,38 +1,48 @@
 ﻿using DaMid.Contexts;
 using DaMid.Models;
+using DaMid.Interfaces;
 
 namespace DaMid.Services {
     public interface IAuthService {
-        public Task<UserModel?> LoginAsync(string email, string password);
-        public Task<UserModel?> RegisterAsync(string email, string password);
+        public Task<string?> LoginAsync(string email, string password);
+        public Task<string?> RegisterAsync(string email, string password);
     }
 
-    public class AuthService(IUserService userService) : IAuthService {
+    public class AuthService(IUserService userService, IJwtService jwtService) : IAuthService {
         private readonly IUserService _userService = userService;
+        private readonly IJwtService _jwtService = jwtService;
 
-        public async Task<UserModel?> LoginAsync(string login, string password) {
+        public async Task<string?> LoginAsync(string login, string password) {
             var user = await _userService.GetUserByLoginAsync(login);
             if (user == null) {
                 return null;
             }
 
-            if (BCrypt.Net.BCrypt.Verify(password, user.Password)) {
-                return user;
+            if (!BCrypt.Net.BCrypt.Verify(password, user.Password)) {
+                return null;
             }
 
-            return null;
+            return _jwtService.GenerateToken(new ITokenPayload {
+                UserId = user.Id,
+                Role = user.Role
+            });
         }
-        public async Task<UserModel?> RegisterAsync(string login, string password) {
+        public async Task<string?> RegisterAsync(string login, string password) {
             var user = await _userService.GetUserByLoginAsync(login);
             if (user != null) {
                 return null;
             }
 
-            return await _userService.CreateUserAsync(new UserModel {
+            var newUser = await _userService.CreateUserAsync(new UserModel {
                 Login = login,
                 Role = UserRole.User,
                 Password = BCrypt.Net.BCrypt.HashPassword(password),
                 RegistrationDate = DateTime.UtcNow
+            });
+
+            return _jwtService.GenerateToken(new ITokenPayload {
+                UserId = newUser.Id,
+                Role = newUser.Role
             });
         }
     }
