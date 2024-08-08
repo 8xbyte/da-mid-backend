5861245ab4a2f9e325bf424b1894f414fc038259
micmaclaynd <101046221+micmaclaynd@users.noreply.github.com>
2024-08-08 11:39:14 +0300

    Update startup code Configure cookie options

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.Configure<IJwtOptions>(builder.Configuration.GetSection("Jwt"));
 builder.Services.Configure<IDatabaseOptions>(builder.Configuration.GetSection("Database"));
+builder.Services.Configure<ICookieOptions>(builder.Configuration.GetSection("Cookie"));
 
 builder.Services.AddScoped<IJwtService, JwtService>();
 builder.Services.AddScoped<IUserService, UserService>();
