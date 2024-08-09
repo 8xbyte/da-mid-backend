f6383530c1406929a03e5e8f04f0ab28002ad63e
micmaclaynd <101046221+micmaclaynd@users.noreply.github.com>
2024-08-09 12:20:19 +0300

    Add "UpdateClassModel" migration

---
+++ Migrations/20240809091650_UpdateClassModel.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DaMid.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateClassModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "group_id",
+                table: "classes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_classes_group_id",
+                table: "classes",
+                column: "group_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_classes_groups_group_id",
+                table: "classes",
+                column: "group_id",
+                principalTable: "groups",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_classes_groups_group_id",
+                table: "classes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_classes_group_id",
+                table: "classes");
+
+            migrationBuilder.DropColumn(
+                name: "group_id",
+                table: "classes");
+        }
+    }
+}
