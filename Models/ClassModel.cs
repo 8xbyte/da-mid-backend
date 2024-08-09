ab297b5268730e25270908131d27a1c78ac9e815
micmaclaynd <101046221+micmaclaynd@users.noreply.github.com>
2024-08-09 13:44:06 +0300

    Update class model Linking fields are marked as nullable

--- Models/ClassModel.cs
+++ Models/ClassModel.cs
@@ -7,22 +7,22 @@ namespace DaMid.Models {
         [Required]
         [Column("subject_id")]
         public int SubjectId { get; set; }
-        public SubjectModel Subject { get; set; }
+        public SubjectModel? Subject { get; set; }
 
         [Required]
         [Column("audience_id")]
         public int AudienceId { get; set; }
-        public AudienceModel Audience { get; set; }
+        public AudienceModel? Audience { get; set; }
 
         [Required]
         [Column("teacher_id")]
         public int TeacherId { get; set; }
-        public TeacherModel Teacher { get; set; }
+        public TeacherModel? Teacher { get; set; }
 
         [Required]
         [Column("group_id")]
         public int GroupId { get; set; }
-        public GroupModel Group { get; set; }
+        public GroupModel? Group { get; set; }
 
         [Required]
         [Column("date")]
